b5ecd6aeba81b8acd28fb88ee79326c40e41a593
ryan-tyrese <81622319+ryan-tyrese@users.noreply.github.com>
2025-01-27 13:47:07 +0200

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Card(){
+    const [airing, setAiring] = useState([]);
+    
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const response = await axios.get("https://api.jikan.moe/v4/anime?status=airing");
+        console.log("response", response);
+        setAiring(response.data)
+    }
+    
     return(
         <>
             <div className="card bg-slate-100/5 card-side bg-base-100 w-96 shadow-xl">
@@ -9,7 +24,7 @@ export default function Card(){
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">Title</h2>
-                    <p>Rating:9.5  Rank:10  Year:2020 Episodes:36</p>
+                    <p>Rating:9.5  Rank:10  Year:2020  Episodes:36</p>
                     <div className="card-actions">
                         <div className="badge badge-outline badge-primary">genre</div>
                     </div>
